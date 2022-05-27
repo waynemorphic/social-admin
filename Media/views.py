0094a046113a9f7e0d5283c0b036b70640f50b9c
waynemorphic <wayneklyde@gmail.com>
2022-05-27 20:23:33 +0300

    url for the index function and search results

--- Media/views.py
+++ Media/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime as dt
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def search_results(request):
+    return render(request, 'search.html')
