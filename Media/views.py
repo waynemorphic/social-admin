b4eb19ffb3247e7bbf857643e221d61dd2d8f669
waynemorphic <wayneklyde@gmail.com>
2022-05-28 12:07:22 +0300

    views file with date functions

--- Media/views.py
+++ Media/views.py
@@ -6,5 +6,21 @@ from datetime import datetime as dt
 def index(request):
     return render(request, 'index.html')
 
+# post for particular date
+def post_of_day(request):
+    date = dt.date.today()
+    return render(request, {"date": date})
+
+# getting the day of the week
+def convert_dates(dates):
+    # get weekday number for the particular date
+    day_number = dt.date.weekday(dates)
+    
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+    
+    # return the actual day
+    day = days[day_number]
+    return day
+
 def search_results(request):
-    return render(request, 'search.html')
+    return render(request, 'post/search.html')
